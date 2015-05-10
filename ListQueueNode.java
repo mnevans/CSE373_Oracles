ccb7388f6058d428f6f64444964fce4f90cf36ee
mnevans <evansmorg@gmail.com>
2015-05-10 13:06:54 -0700

    Create ListQueueNode.java

---
+++ ListQueueNode.java
@@ -0,0 +1,10 @@
+// Morgan Evans, 1124703, mnevans
+
+public class ListQueueNode {
+   public String data;
+   public ListQueueNode next;
+   
+   public ListQueueNode(String data) {
+      this.data = data;
+   }
+}
